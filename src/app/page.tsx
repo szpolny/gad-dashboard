d319c7a17c3cc6f022e32d524f447471d9ac719b
Szymon <szymonpolny@outlook.com>
2024-03-05 23:49:58 +0100

    Add NextAuth with Discord provider, update dependencies, and refactor layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,40 @@
-import { ModeToggle } from '@/components/ModeToggle';
+'use client';
+
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { FaDiscord } from 'react-icons/fa';
+
 import { Button } from '@/components/ui/button';
 
 const Home = () => {
-  return (
-    <div>
-      <div className="flex w-full h-screen items-center justify-center">
-        <Button>Login with Discord</Button>
+  const { data: session } = useSession();
+
+  if (!session) {
+    return (
+      <div>
+        <div className="flex w-full h-screen items-center justify-center">
+          <div className="text-center">
+            <h1 className="text-2xl m-5 font-semibold">
+              Arma 3 GAD - Server Dashboard
+            </h1>
+            <Button
+              onClick={() => {
+                signIn('discord');
+              }}
+            >
+              Login with Discord <FaDiscord className="ml-2" />
+            </Button>
+          </div>
+        </div>
       </div>
-      <div className="absolute right-5 top-5">
-        <ModeToggle />
+    );
+  } else {
+    return (
+      <div>
+        <p>You are logged in as {session.user!.name}!</p>
+        <Button onClick={() => signOut()}>Sign out</Button>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Home;
